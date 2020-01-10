2be688cc8f5f989979c282e95c064642963d70fa
Gyeongsik Kim <admin@gyungdal.cc>
2020-01-11 03:37:18 +0900

    세션 저장안되는 버그 수정

--- src/index.js
+++ src/index.js
@@ -1,31 +1,53 @@
 const configs =  require("./setting");
 const express = require("express");
 const session = require("express-session");
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
-
-
-console.log(configs);
-
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
+const sessionStore = session({
     secret: configs.sessionKey,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
-}))
+    cookie: { 
+        maxAge: 36000 
+    },
+    store: FileStore()
+});
+console.log(configs);
 
+app.set('trust proxy', 1);
+
+//세션 설정
+app.use(sessionStore);
+
+//쿠키 및 바디 파서 미들 웨어 추가
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+//js랑 css는 정적루트로 라우팅
 app.use('/js', express.static("./web/js"));
 app.use('/css', express.static("./web/css"));
 
 // /user 로 시작되는 경로는 전부 user.js 안에 있는 핸들러로 라우팅
 const userHandler = require("./user");
+userHandler.use(sessionStore);
 app.use("/user", userHandler);
 
-//테스트용 Hello!
-app.get("/", (req, res)=>{
-    res.send("Hello!");
+//테스트용!
+app.get('/', function (req, res, next) {
+    console.log(req.session);
+    if(req.session.num === undefined){
+        req.session.num = 1;
+    } else {
+        req.session.num =  req.session.num + 1;
+    }
+    req.session.save(()=>{
+        res.send(`Views : ${req.session.num}`);
+    });
 });
 
 app.listen(configs.port, () => {
-	console.log(`[INFO] ${configs.port} Start`);		
+	console.log(`[INFO] Port ${configs.port} Start`);		
 });
--- src/user/index.js
+++ src/user/index.js
@@ -1,18 +1,45 @@
 const { Router } = require("express");
 const router = Router();
+const multer = require("multer");
+const formParser = multer();
 
-router.get('/', (req, res) => {
-    res.send(`your ID : ${req.session.id}`);
-});
+function requireAuthentication (req, res, next) {
+	if (!req.session || !req.session.authenticated) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+	next();
+}
 
-router.get('/login', (req, res)=> {
-    console.log(`${__dirname}/login.html`);
-    res.sendFile(`${__dirname}/login.html`);
+router.route("/")
+    .get((req, res) => {
+        console.log(req.session);
+        if(req.session.authenticated){
+            res.send(`Your ID : ${req.session.email}`);
+        }else{
+            res.redirect("/user/login");
+        }
+    });
+
+router.get("/login", (req, res)=> {
+        console.log(`${__dirname}/login.html`);
+        res.sendFile(`${__dirname}/login.html`);
+    });
+router.post("/login", formParser.none(), (req, res)=>{
+        console.log(req.body);
+        req.session.email = req.body.email;
+        req.session.password = req.body.password;
+        req.session.authenticated = true;
+        console.log(req.session);
+        res.redirect("/user");
+    });
+
+router.get("/logout", (req, res)=>{
+    req.session.destroy();
 });
 
 router.get('/register', (req, res)=> {
-    console.log(`${__dirname}/login.html`);
-    res.redirect(`${__dirname}/login.html`);
+
 });
 
 module.exports = router;
